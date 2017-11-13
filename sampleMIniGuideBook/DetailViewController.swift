cdc7310ae77314a6c06e9d0805ef8bc208f78012
Taiki__ck <tensainanoda@gmail.com>
2017-11-13 08:48:12 +0800

    宿題コミット

---
+++ sampleMIniGuideBook/DetailViewController.swift
@@ -0,0 +1,86 @@
+//
+//  DetailViewController.swift
+//  sampleMIniGuideBook
+//
+//  Created by 加藤　大起 on 2017/11/12.
+//  Copyright © 2017年 Taiki Kato. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var myMapView: MKMapView!
+    
+    var movedPlace:String = ""
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        
+        //ファイルパス取得
+        let filePath = Bundle.main.path(forResource: "areaList", ofType: "plist")
+        
+        //ファイル読み込み && Dic型に代入
+        let dic = NSDictionary(contentsOfFile: filePath!)
+        
+        // plistデータをDic型に変換
+        for(key,data) in dic!{
+            let dicData:NSDictionary = data as! NSDictionary
+            if((key as! String) as String == movedPlace){
+                navigationItem.title = movedPlace
+                //説明文代入
+                myLabel.text = dicData["description"] as! String
+                //画像代入
+                myImageView.image = UIImage(named:dicData["image"] as! String)
+                //緯度代入
+                let latitude:String = dicData["latitude"] as! String
+                //経度代入
+                let longitude:String = dicData["longitude"] as! String
+                //地域定義
+                let coordinate = CLLocationCoordinate2DMake(atof(latitude), atof(longitude))
+                //縮尺定義
+                let span = MKCoordinateSpanMake(0.1, 0.1)
+                //地域・縮尺代入
+                let region = MKCoordinateRegionMake(coordinate, span)
+                myMapView.setRegion(region, animated:true)
+                
+                // ピン定義
+                let myPin: MKPointAnnotation = MKPointAnnotation()
+                // 座標決め
+                myPin.coordinate = coordinate
+                // pinタイトル
+                myPin.title = movedPlace
+                
+                // pinサブタイトル
+                myPin.subtitle = "おすすめ"
+                // マップにピン表示
+                myMapView.addAnnotation(myPin)
+            }
+            
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- sampleMIniGuideBook/ViewController.swift
+++ sampleMIniGuideBook/ViewController.swift
@@ -8,18 +8,89 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    @IBOutlet weak var myTableView: UITableView!
+    var areaList:[String] = []
+    
+    var selectedPlace:String = ""
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationItem.title = "セブ一覧"
+        
+        //ファイルパスを取得（エリア名が格納されているプロパティリスト）
+        let filePath = Bundle.main.path(forResource: "areaList", ofType: "plist")
+        
+        //ファイルの内容を読み込んでディクショナリー型に格納
+        let dic = NSDictionary(contentsOfFile: filePath!)
+        
+        //TableViewで扱いやすい配列の形（エリア名の入ってる配列）を作成
+        for (key, data) in dic! {
+            print(key)
+            areaList.append(key as! String)
+        }
+        
     }
-
+    
+    //2.行数決定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return areaList.count
+    }
+    
+    //3.表示する文字列を決定
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //文字列を表示するセルの取得
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        //文字色を青にする
+        cell.textLabel?.textColor = UIColor.blue
+        
+        //表示したい文字の設定
+        cell.textLabel?.text = areaList[indexPath.row]
+        
+        //文字を設定したセルを返す
+        return cell
+    }
+    
+    //選択された行取得
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\((indexPath as NSIndexPath).row)行目選択")
+        selectedPlace = areaList[(indexPath as NSIndexPath).row] as String
+        performSegue(withIdentifier: "showDetail",sender: nil)
+    }
+    
+    //セグエに入れてDetailに飛ばす
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let toDetail = segue.destination as! DetailViewController
+        
+        toDetail.movedPlace = selectedPlace
+        
+    }
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
