a4a1d641b6fcde68631745bba71042732504c520
Taiki__ck <tensainanoda@gmail.com>
2017-11-13 11:31:10 +0800

    提出

--- sampleMIniGuideBook/DetailViewController.swift
+++ sampleMIniGuideBook/DetailViewController.swift
@@ -12,7 +12,9 @@ import MapKit
 
 class DetailViewController: UIViewController {
 
+    
     @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myTextView: UITextView!
     @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var myMapView: MKMapView!
     
@@ -20,7 +22,16 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         
+        //タイトルをナビゲーションバーに表示
+        
+        
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.gray
+        myTextView.backgroundColor = UIColor.black
+        myTextView.textColor = UIColor.white
+        //値の受取確認
+        print(movedPlace)
+        myLabel.text = movedPlace
         
         
         //ファイルパス取得
@@ -28,6 +39,8 @@ class DetailViewController: UIViewController {
         
         //ファイル読み込み && Dic型に代入
         let dic = NSDictionary(contentsOfFile: filePath!)
+        //(今画面に表示したいデータの取得)
+        //let detailnfo = dic![getName] as! NSDictionary
         
         // plistデータをDic型に変換
         for(key,data) in dic!{
@@ -35,28 +48,30 @@ class DetailViewController: UIViewController {
             if((key as! String) as String == movedPlace){
                 navigationItem.title = movedPlace
                 //説明文代入
-                myLabel.text = dicData["description"] as! String
+                myTextView.text = dicData["description"] as! String
                 //画像代入
                 myImageView.image = UIImage(named:dicData["image"] as! String)
+                
+                
                 //緯度代入
                 let latitude:String = dicData["latitude"] as! String
                 //経度代入
                 let longitude:String = dicData["longitude"] as! String
-                //地域定義
+                //中心定義
+                //型変換　String->Double型
                 let coordinate = CLLocationCoordinate2DMake(atof(latitude), atof(longitude))
                 //縮尺定義
                 let span = MKCoordinateSpanMake(0.1, 0.1)
-                //地域・縮尺代入
+                //範囲object
                 let region = MKCoordinateRegionMake(coordinate, span)
+                //マップ表示
                 myMapView.setRegion(region, animated:true)
-                
                 // ピン定義
                 let myPin: MKPointAnnotation = MKPointAnnotation()
                 // 座標決め
                 myPin.coordinate = coordinate
                 // pinタイトル
                 myPin.title = movedPlace
-                
                 // pinサブタイトル
                 myPin.subtitle = "セブで熱い場所"
                 // マップにピン表示
--- sampleMIniGuideBook/ViewController.swift
+++ sampleMIniGuideBook/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     @IBOutlet weak var myTableView: UITableView!
     var areaList:[String] = []
     
+    //選択されたエリア名を保存するメンバ変数
     var selectedPlace:String = ""
 
     
@@ -56,41 +57,27 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         return cell
     }
     
-    //選択された行取得
+    //選択された行取得(タップされたとき)
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("\((indexPath as NSIndexPath).row)行目選択")
+        //タップされた行のエリア名を保存
         selectedPlace = areaList[(indexPath as NSIndexPath).row] as String
+        //セグエのidentifierを指定して、画面移動
         performSegue(withIdentifier: "showDetail",sender: nil)
     }
     
     //セグエに入れてDetailに飛ばす
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //次の画面のインスタンスを取得
         let toDetail = segue.destination as! DetailViewController
-        
+        //次の画面のプロパティにタップされた行のエリア名を渡す
         toDetail.movedPlace = selectedPlace
         
     }
-    
-    
-    
-    
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
